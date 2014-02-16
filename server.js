02711f1e5c0ac75c59d9484ab55d817e931a3e4e
Phil Corbett <corbett.phil@gmail.com>
2014-02-16 08:38:16 -0500

    structure updates

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
 'use strict';
 
-var express = require('express'),
-    app = express();
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-app.get('/', function (req, res) {
-    res.send('hello again 333!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Express app started on port ' + port);
-console.log('__dirname ' + __dirname);
+module.exports = require('./lib/dosa-server-node');
